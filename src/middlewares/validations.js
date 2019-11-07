fd97f5b0adb9507af21b2baa439754b34e9fe67b
filschristian <filschristian94@gmail.com>
2019-11-07 14:13:34 +0200

    validation improvements

--- src/middlewares/validations.js
+++ src/middlewares/validations.js
@@ -1,22 +1,36 @@
 import joi from 'joi';
 
-export const validateIdParam = (req, res, next) => {
-  const validationErrors = joi.validate(req.params.id, joi.number().integer());
+const validateSchema = (data, schema) => {
+  const validationErrors = joi.validate(data, schema);
   if (validationErrors.error) {
+    const { details } = validationErrors.error;
+    const message = details.map((i) => i.message).join(',');
+    return message;
+  }
+  return false;
+};
+
+export const validateIdParam = (req, res, next) => {
+  const schema = joi.object().keys({
+    id: joi.number().integer()
+  });
+  const validationError = validateSchema(req.params, schema);
+  if (validationError) {
     return res.status(400)
-      .send({ errors: validationErrors });
+      .send({ error: validationError });
   }
   return next();
 };
 
 export const validateGetAllDrivers = (req, res, next) => {
   if (req.query.status) {
-    const validationErrors = joi.validate(req.query, joi.object().keys({
+    const schema = joi.object().keys({
       status: joi.string().trim().valid(['available', 'on duty']),
-    }));
-    if (validationErrors.error) {
+    });
+    const validationError = validateSchema(req.query, schema);
+    if (validationError) {
       return res.status(400)
-        .send({ errors: validationErrors });
+        .send({ error: validationError });
     }
     return next();
   }
@@ -24,38 +38,41 @@ export const validateGetAllDrivers = (req, res, next) => {
 };
 
 export const validateTripBody = (req, res, next) => {
-  const validationErrors = joi.validate(req.body, joi.object().keys({
+  const schema = joi.object().keys({
     riderId: joi.number().integer().required(),
     driverId: joi.number().integer().required(),
     pickup: joi.string().required(),
     destination: joi.string().required()
-  }));
-  if (validationErrors.error) {
+  });
+  const validationError = validateSchema(req.body, schema);
+  if (validationError) {
     return res.status(400)
-      .send({ errors: validationErrors });
+      .send({ error: validationError });
   }
   return next();
 };
 
 export const validateCompleteTripBody = (req, res, next) => {
-  const validationErrors = joi.validate(req.body, joi.object().keys({
+  const schema = joi.object().keys({
     cost: joi.number().required()
-  }));
-  if (validationErrors.error) {
+  });
+  const validationError = validateSchema(req.body, schema);
+  if (validationError) {
     return res.status(400)
-      .send({ errors: validationErrors });
+      .send({ error: validationError });
   }
   return next();
 };
 
 export const validateLocation = (req, res, next) => {
-  const validationErrors = joi.validate(req.query, joi.object().keys({
+  const schema = joi.object().keys({
     latitude: joi.number().min(-90).max(90).required(),
     longitude: joi.number().min(-180).max(180).required()
-  }));
-  if (validationErrors.error) {
+  });
+  const validationError = validateSchema(req.query, schema);
+  if (validationError) {
     return res.status(400)
-      .send({ errors: validationErrors });
+      .send({ error: validationError });
   }
   return next();
 };
